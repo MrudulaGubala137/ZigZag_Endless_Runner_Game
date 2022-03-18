3419d055ed3528890bb6cd261fb27ef0b47e9805
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-03-18 16:28:26 +0530

    Playermovement and random tile spawning in right and forward direction is done.

---
+++ Assets/Scripts/TileSpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public GameObject rightTile;
+   public GameObject currentTile;
+    public GameObject forwardTile;
+    public GameObject[] tilesPrefab;
+    int count = 10;
+    void Start()
+    {
+        for (int i = 0; i < count; i++)
+        {
+          int k = Random.Range(0, 2);
+            //Instantiate(rightTile,currentTile.transform.GetChild(1).position,Quaternion.identity);
+            currentTile=Instantiate(tilesPrefab[k], currentTile.transform.GetChild(k).position, Quaternion.identity);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
