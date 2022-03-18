43517274658db3bd00fa8a4ac4b30442da1c73b5
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-03-18 15:54:38 +0530

    Background,Platform and player created. And added tile points to spawn.

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerSpeed;
+    Vector3 direction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(direction==Vector3.forward)
+            {
+                direction = Vector3.left;
+            }
+            else
+            {
+                direction = Vector3.forward;
+            }
+        }
+        transform.Translate(direction*playerSpeed*Time.deltaTime);
+
+    }
+}
